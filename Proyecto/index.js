ccc8f2e618306eadb5df1b7e2a084459ecdb0be6
Robert Smart <roca1388@gmail.com>
2021-11-11 12:45:16 -0300

    Archivo principal del Modulo

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const debug = require('debug')('app:main')
 
-const {Config} =require("./src/config/index");
+const {Config} = require("./src/config/index");
+const {ProductsAPI} = require('./src/products/index');
 const app = express();
 
 app.use(express.json());
+
+ProductsAPI(app)
 
 //modulos
 var PORT=parseInt(Config.port);
--- Proyecto/src/products/index.js
+++ Proyecto/src/products/index.js
@@ -1,1 +1,12 @@
 const express = require('express');
+
+const router = express.Router();
+
+module.exports.ProductsAPI = (app)=>{
+    router
+    .get('/', (req, res)=>{}) // http://localhost:3000/api/products/
+    .get('/:id', (req, res)=>{})// http://localhost:3000/api/products/23
+    .post('/', (req, res)=>{});
+
+    app.use('/api/products', router)
+}
